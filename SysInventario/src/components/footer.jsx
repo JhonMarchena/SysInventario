0ae219d99a0b3919f676c1cec9e1dd9e147a5f02
JhonMarchena <jhcronos@gmail.com>
2024-05-12 22:34:21 -0400

    ajustes en diseño del footer

--- SysInventario/src/components/footer.jsx
+++ SysInventario/src/components/footer.jsx
@@ -7,7 +7,7 @@ function Footer() {
     <footer className='footerhp'>
       <section className="luissc">
       <span className='namesc'>
-      <h1>Luis Angel Calderon</h1>
+      <h1>Luis Calderon</h1>
       <h2>Frontend Developer</h2>
       </span>
       <h2>Luiscal14.2</h2>
@@ -15,7 +15,7 @@ function Footer() {
 
       <section className="jhonsc">
       <span className='namesc'>
-      <h1>Jhon Alexander Marchena</h1>
+      <h1>Jhon Marchena</h1>
       <h2>Frontend Developer</h2>
       </span>
       <h2>Jhcronos</h2>
