7120a0ced6214fcc25eb42c061b5223015b5b2d8
JhonMarchena <jhcronos@gmail.com>
2024-06-12 22:36:03 -0400

    Integración de maquetación About Us

--- SysInventario/src/components/mainau.jsx
+++ SysInventario/src/components/mainau.jsx
@@ -1,14 +1,29 @@
 import React from 'react'
+import '../styles/aboutus.css'
 
 function mainau() {
   return (
-    <div>
-    <main className='main'>
-    <section className="cointenerwmd">What me Do</section>
-    <section className="cointener"></section>
-    <section className="cointener"></section>
-    </main>
- 
+    <div className='main'>
+    <section className='conteinermn'>
+    <section className="cointener1">
+    <h1>Lo que hago</h1>
+    <h2>
+    Como desarrollador frontend, diseño y desarrollo la parte visual de las aplicaciones web. 
+    Mi trabajo incluye estructurar el contenido usando HTML, aplicar estilos con CSS para hacer 
+    las páginas atractivas y coherentes, y añadir interactividad mediante JavaScript para mejorar 
+    la experiencia del usuario. Utilizando tecnologías como: React, ViteJs, Node.js, MongoDB, etc...
+    </h2>
+    </section>
+    <section className="conteiner2">
+    <h1>Quien soy</h1>
+    <h2>
+    ¡Hola! Soy Jhon Alexander Marchena Sánchez, una persona apasionada por el aprendizaje continuo y 
+    comprometida con alcanzar mis objetivos. Desde temprana edad, he cultivado un profundo interés por 
+    adquirir nuevos conocimientos y habilidades, siempre buscando mejorar y superarme a mí mismo en todos 
+    los aspectos de mi vida.
+    </h2>
+    </section>
+    </section>
     </div>
   )
 }
